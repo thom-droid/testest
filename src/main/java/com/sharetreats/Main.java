11fed514984ea704f43551d22e24c64393d7ce28
thom-window <hsbang.thom@gmail.com>
2023-05-24 17:01:43 +0900

    initial

---
+++ src/main/java/com/sharetreats/Main.java
@@ -0,0 +1,8 @@
+package com.sharetreats;
+
+public class Main {
+
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+}
---
+++ src/test/java/com/sharetreats/MainTest.java
@@ -0,0 +1,14 @@
+package com.sharetreats;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class MainTest {
+
+    @Test
+    public void add() {
+        Main main = new Main();
+    }
+}
